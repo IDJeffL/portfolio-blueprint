4c3cb13af61a96a24f66575b8d0dda8b5c89bee9
IDJeffL <105044603+IDJeffL@users.noreply.github.com>
2023-01-04 12:08:16 +0000

    Update EditHtml.js

--- components/EditHtml/EditHtml.js
+++ components/EditHtml/EditHtml.js
@@ -13,54 +13,6 @@ export default function EditHtml( { postId } ) {
    */
   const WORDPRESS_URL=process.env.NEXT_PUBLIC_WORDPRESS_URL
 
-  /**
-   * Set user status.
-   */
-  let [userStatus, getUserData] = useState({});
-  let [userStatusJWT, getUserDataJWT] = useState({});
-
-  /**
-  * Get user details from localStorage obj. 
-  */
-  useEffect(()=> {
-    getUserData(localStorage.getItem('user'));
-  }, [])
-
-  /**
-   * Get user JWT details from localStorage obj 
-   */
-  useEffect(()=> {
-    getUserDataJWT(localStorage.getItem('userJWT'))
-  }, [])
-
-  /**
-   * If user is not logged in, dont display EditHTML options.
-   * ( in progress, we need to check whose allowed to use this, etc ... )
-   */
-  if ( userStatus === null ) {
-    return(<></>)
-  }
-
-  /* Set default value(s) */
-  let editMode = false;
-
-  /* Enable edit mode */
-  if ( typeof document !== 'undefined' 
-        && document.getElementsByClassName('enableEditHtml')[0] ) {
-    document.getElementsByClassName('enableEditHtml')[0].onclick = function(){
-      document.designMode = 'on'
-      editMode = true
-    }
-  }
-
-  /* Disable edit mode */
-  if ( typeof document !== 'undefined' 
-        && document.getElementsByClassName('saveEditHtml')[0] ) {
-    document.getElementsByClassName('saveEditHtml')[0].onclick = function(){
-      document.designMode = 'off'
-    }
-  }
-
 
 
   /* Return content */
