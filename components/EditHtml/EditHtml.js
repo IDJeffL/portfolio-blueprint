a0da38931734c8af514d604944540fa81105f26c
IDJeffL <105044603+IDJeffL@users.noreply.github.com>
2023-01-06 12:48:25 +0000

    Update EditHtml.js

--- components/EditHtml/EditHtml.js
+++ components/EditHtml/EditHtml.js
@@ -12,53 +12,54 @@ export default function EditHtml( { postId } ) {
    * ( this should also be configured in the .env.local file )
    */
   const WORDPRESS_URL=process.env.NEXT_PUBLIC_WORDPRESS_URL
+  
+    /* Options */
+  if ( typeof document !== 'undefined' ) {
+    document.addEventListener('click', function(e) {
 
-  /**
-   * Set user status.
-   */
-  let [userStatus, getUserData] = useState({});
-  let [userStatusJWT, getUserDataJWT] = useState({});
+      /* Set update array */
+      let postUpdates = []
 
-  /**
-  * Get user details from localStorage obj. 
-  */
-  useEffect(()=> {
-    getUserData(localStorage.getItem('user'));
-  }, [])
+      /* Disable/Save */
+      if ( e.target.className === 'saveEditHtml' && editMode === true ) {
+        let editSections = document.querySelectorAll('[class*="editHtmlOn"]');
 
-  /**
-   * Get user JWT details from localStorage obj 
-   */
-  useEffect(()=> {
-    getUserDataJWT(localStorage.getItem('userJWT'))
-  }, [])
+        for( let i = 0; i < editSections.length; i++ ) {
+          /* Remove editing styling effects */
+          editSections[i].removeAttribute('style')
 
-  /**
-   * If user is not logged in, dont display EditHTML options.
-   * ( in progress, we need to check whose allowed to use this, etc ... )
-   */
-  if ( userStatus === null ) {
-    return(<></>)
-  }
+          /* Add object value to array for updating */
+          postUpdates.push( editSections[i].outerHTML )
+        } 
 
-  /* Set default value(s) */
-  let editMode = false;
+        /*
+         * Send to WP via REST API
+         */
+        const apiURL = WORDPRESS_URL + '/wp-json/fe-text-editor/v1/posts/' + postId + '?jwt=' + userStatusJWT
+        const response = fetch(apiURL, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(postUpdates)
+            })
 
-  /* Enable edit mode */
-  if ( typeof document !== 'undefined' 
-        && document.getElementsByClassName('enableEditHtml')[0] ) {
-    document.getElementsByClassName('enableEditHtml')[0].onclick = function(){
-      document.designMode = 'on'
-      editMode = true
-    }
-  }
+        /* Edit mode false */
+        editMode = false
 
-  /* Disable edit mode */
-  if ( typeof document !== 'undefined' 
-        && document.getElementsByClassName('saveEditHtml')[0] ) {
-    document.getElementsByClassName('saveEditHtml')[0].onclick = function(){
-      document.designMode = 'off'
-    }
+        /* Exit here ... */
+        return
+      } 
+
+      /* Highlite edit fields */
+      if ( e.target.className === 'enableEditHtml' && editMode === false ) {
+        let section = document.querySelectorAll('[class*="editHtmlOn"]');
+        for( let i = 0; i < section.length; i++ ) {
+          section[i].style.border = '1px solid green'
+        } 
+      }
+
+    }, false);  
   }
   
   /* Return content */
